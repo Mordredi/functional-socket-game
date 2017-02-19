c8179c22f3cf4f71f774035074b50d0214df4181
Arthur <arthur.p.wright@gmail.com>
2017-02-19 08:15:25 -0500

    figured out click event issue

--- src/js/manipulations/index.js
+++ src/js/manipulations/index.js
@@ -4,20 +4,35 @@ import { timer } from '../components/timer'
 import { IO } from 'ramda-fantasy'
 import { Observable } from 'rxjs/Rx'
 
-const flickrClick$ = click$(querySelectorAll('.flickr-image').runIO())
-    //.map(e => e.target.id)
-    //.take(1)
 
 export const gameTimer = container => IO(() => {
+
+  const flickrClick$ = click$(querySelectorAll('.flickr-image').runIO())
+    .map(e => e.target.id)
+    .take(1)
+    .map(x => {
+      console.log(`click ${x}`)
+      return x
+    }
+    )
+
+  tenSeconds$
+    .map(number => {
+      console.log(`timer ${number}`)
+      replaceElement(timer({number}), querySelector('.timer-number'), container).runIO()
+    })
+
+
   Observable.combineLatest(
     tenSeconds$,
-    flickrClick$
+    flickrClick$,
+    (number, id) => ({number, id})
+
   )
     .subscribe(
-      (x) => {
-        console.log(x)
-
-        return replaceElement(timer({number: x.number}), querySelector('.timer-number'), container).runIO()},
+      ({number, id}) => {
+        console.log(number, id)
+      },
       console.log,
       (x) => console.log(x)
     )
